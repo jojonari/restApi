0ae9614e8f1abf2b0d1a407606ff25ee2553f3b6
jojonari <xkgun@naver.com>
2019-11-08 22:08:47 +0900

    주석삭제

--- src/test/java/com/rest/api/repo/UserJpaRepoTest.java
+++ src/test/java/com/rest/api/repo/UserJpaRepoTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+//test
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserJpaRepoTest {
