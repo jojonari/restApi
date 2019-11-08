386a3490c466227084696ed96a22293d76d7f419
jojonari <xkgun@naver.com>
2019-11-08 21:53:44 +0900

    A브랜치 커밋

--- src/test/java/com/rest/api/repo/UserJpaRepoTest.java
+++ src/test/java/com/rest/api/repo/UserJpaRepoTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+//마스터 주석
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserJpaRepoTest {
